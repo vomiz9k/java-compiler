db62ce8f7f19dc33e7184c0e88e6d49563d93797
vomiz9k <kiazimov.ka@phystech.edu>
2021-04-14 23:47:32 +0300

    tests for scope tree

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ### build: in root dir:
 ## cmake ./ && make
 ### run:
-## ./Compiler input-filename [_dot-output-filename_ OR _run_]
+## ./Compiler input-filename [run/ast/scope] [_dot-output-filename_ if ast]
 ##
-### if you have graphviz(dot) installed:
-## ./test_tree.sh
-### and you can see cool derevya in .png format in tests/tree_making
+### test print ast:
+## ./test_ast.sh
+### test print scope tree:
+## ./test_scope.sh
--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <driver.hpp>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[2])
 {
     Driver d;
     d.parse(argv[1]);
-    // if (argc == 3)
-    // {
-    //     if(argv[2] == "run")
-    //         d.interpret();
-    //     else
-    //         d.print(argv[2]);
-    // }
-    d.symb();
+    std::string cmd = argv[2];
+    if(cmd == "run") {
+        d.interpret();
+    }
+    else if (cmd == "ast") {
+        d.print(argv[3]);
+    }
+    else if (cmd == "scope") {
+        d.symb();
+    }    
 } 
